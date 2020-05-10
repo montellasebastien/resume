28fd32e8ea013253b9a5bdb8ee14f2ac1f17ae76
montellasebastien <montella.sebastien@gmail.com>
2020-05-10 22:15:03 +0200

    filling experiences.

--- experience.py
+++ experience.py
@@ -11,19 +11,6 @@ class SebExperience:
         self.current_experience_year = None
         self.current_group = None
 
-    def widm_lab(self, is_first=0):
-
-        self.introduce_job_position("Research Assistant - WIDM Lab", '(2016-2019)', is_first=is_first)
-        self.verso()
-        self.us_election_prediction()
-        self.ntcir_13()
-        self.qnap_user_simulator()
-        self.taiwan_iii()
-        self.ntcir_14()
-
-    def orange_intern(self):
-        self.introduce_job_position("Research Intern - Orange Labs", '(Feb. 2019 - August 2019)')
-
     def introduce_job_position(self, title_string, period_string, is_first=0):
         experience_title = TextMobject("\\textit{ " + title_string +"}")
         experience_title.scale(0.7)
@@ -61,20 +48,69 @@ class SebExperience:
             self.current_experience_year = experience_year
             self.current_group = new_group
 
-    def verso(self):
-        print("haha")
+    def widm_lab(self, is_first=0):
 
-    def us_election_prediction(self):
-        print('')
+        self.introduce_job_position("Research Assistant - WIDM Lab", '(2016-2019)', is_first=is_first)
 
-    def ntcir_13(self):
-        print('haha')
+        verso_object = self.create_experience('$\\cdot \\text{ Facebook Data Extraction}$')
+        verso_object.move_to(self.current_experience_year.get_center() + DOWN)
 
-    def ntcir_14(self):
-        print('haha')
+        election_object = self.create_experience('$\\cdot \\text{ U.S Election Forecasting}$')
+        election_object.move_to(verso_object.get_center() + DOWN)
 
-    def qnap_user_simulator(self):
-        print("haha")
+        ir_chatbot_object = self.create_experience('$\\cdot \\text{ Retrieval-Based Chatbot}$')
+        ir_chatbot_object.move_to(election_object.get_center() + DOWN)
 
-    def taiwan_iii(self):
-        print('haha')
+        rec_sys_object = self.create_experience('$\\cdot \\text{ Recommendation System}$')
+        rec_sys_object.move_to(ir_chatbot_object.get_center() + DOWN)
+
+        emotional_machine = self.create_experience('$\\cdot \\text{ Emotionally-Triggered Conversational Agent}$')
+        emotional_machine.move_to(rec_sys_object.get_center() + DOWN)
+
+        # PLAYING THE ANIMATIONS
+        self.seb_resume.play(FadeInFromDown(verso_object))
+        self.seb_resume.wait(1.5)
+        self.seb_resume.play(FadeInFromDown(election_object))
+        self.seb_resume.wait(1.5)
+        self.seb_resume.play(FadeInFromDown(ir_chatbot_object))
+        self.seb_resume.wait(1.5)
+        self.seb_resume.play(FadeInFromDown(rec_sys_object))
+        self.seb_resume.wait(1.5)
+        self.seb_resume.play(FadeInFromDown(emotional_machine))
+        self.seb_resume.wait(2)
+
+        # REMOVING THE OBJECTS
+        self.seb_resume.play(FadeOut(verso_object),
+                             FadeOut(election_object),
+                             FadeOut(ir_chatbot_object),
+                             FadeOut(rec_sys_object),
+                             FadeOut(emotional_machine))
+        self.seb_resume.wait(1.5)
+
+    def orange_intern(self):
+        self.introduce_job_position("Research Intern - Orange Labs", '(Feb. 2019 - August 2019)')
+
+        in_out_classification = self.create_experience("$\\cdot \\text{ Indoor-Outdoor Classification in 5G context}$", size=0.80)
+        in_out_classification.move_to(self.current_experience_year.get_center() + 1.5 * DOWN)
+
+        nlp_intern = self.create_experience("$\\cdot \\text{ Study on Graph Embeddings for Language Generation}$", size=0.80)
+        nlp_intern.move_to(in_out_classification.get_center() + 2 * DOWN)
+
+        # PLAYING THE ANIMATIONS
+        self.seb_resume.play(FadeInFromDown(in_out_classification))
+        self.seb_resume.wait(1.5)
+        self.seb_resume.play(FadeInFromDown(nlp_intern))
+        self.seb_resume.wait(2)
+
+        # REMOVING THE OBJECTS
+        self.seb_resume.play(FadeOut(in_out_classification),
+                             FadeOut(nlp_intern))
+        self.seb_resume.wait(1.5)
+
+    def create_experience(self, text, size=0.80, color=WHITE):
+        description = TextMobject(text)
+        description.scale(size)
+        description.set_color(color)
+        return description
+
+
